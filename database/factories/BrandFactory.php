f424d3be5ca717b28d905db31e31317bc837203c
Ayange Felix <ayangefelix8@gmail.com>
2022-02-26 23:05:12 +0100

    add brand and category seeders

---
+++ database/factories/BrandFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Brand;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BrandFactory extends Factory
+{
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Brand::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'uuid' => Str::uuid(),
+            'title' => $this->faker->sentence(2),
+            'slug' => Str::slug($this->faker->sentence(2)),
+        ];
+    }
+}
