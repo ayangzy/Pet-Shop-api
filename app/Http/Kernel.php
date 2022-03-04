e79c2c6de30b7d2e3de92063c2a18827bf16e48e
Ayange Felix <ayangefelix8@gmail.com>
2022-03-04 11:45:17 +0100

    ide helper generator

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
+        'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
         'is_admin' => \App\Http\Middleware\IsAdminMiddleware::class,
         'is_user' => \App\Http\Middleware\IsUserMiddleware::class,
     ];
